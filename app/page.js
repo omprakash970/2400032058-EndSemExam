9561e2343128e94eb3044afaffe9688c542a3ec7
Bandi Om Prakash <omprakashbandi583@gmail.com>
2025-11-28 16:47:15 +0530

    done

---
+++ app/page.js
@@ -0,0 +1,42 @@
+export default function Home() {
+
+  const student = {
+    name: "John Doe",
+    roll: "22CS105",
+    department: "Computer Science"
+  };
+
+  return (
+    <div style={{
+      maxWidth: "400px",
+      margin: "50px auto",
+      padding: "20px",
+      textAlign: "center",
+      border: "1px solid #ddd",
+      borderRadius: "10px",
+      fontFamily: "Arial",
+      background: "#fafafa"
+    }}>
+
+    
+      <img 
+        src="/public/OIP.webp"
+        alt="Profile"
+        style={{
+          width: "150px",
+          height: "150px",
+          borderRadius: "50%",
+          objectFit: "cover",
+          marginBottom: "20px"
+        }}
+      />
+
+      <h2 style={{marginBottom: "15px"}}>Student Details</h2>
+
+      <p><strong>Name:</strong> {student.name}</p>
+      <p><strong>Roll Number:</strong> {student.roll}</p>
+      <p><strong>Department:</strong> {student.department}</p>
+
+    </div>
+  );
+}
